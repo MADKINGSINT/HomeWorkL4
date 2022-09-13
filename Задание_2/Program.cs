34165c048f28206dcbbe55131dcee0d7566a595e
madkings <unconfigured@null.spigotmc.org>
2022-09-13 11:44:05 +0300

    Programs writed

---
+++ Задание_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+
+Console.Write("введите число:    ");
+ string WritedNumber = (Console.ReadLine());
+
+ int a = int.Parse(WritedNumber);
+            int sum = 0;
+            while (a > 0)
+            {
+ 
+                sum = sum + a % 10;
+                a = a /10 ;
+ 
+            }
+ Console.WriteLine(sum);
---
+++ Задание_3/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] GetMassiv ()
+{
+Random rnd = new Random();
+int[] array = new int[8];
+for(int i = 0; i < array.Length; i++)
+{
+     array[i] = rnd.Next(0, 10);
+}
+Console.WriteLine(array);
+return array;
+}
+int[] arr = GetMassiv();
+void printArray(int[]array)
+{
+    for  (int i = 0; i<array.Length; i++  )
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+}
+
+printArray(arr);
+Console.WriteLine("");
+
+
